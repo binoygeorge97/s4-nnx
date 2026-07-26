51c0c02633e27a005482648358bd7a22c3b0cf2a
Binoy Mathai George <121314329+binoygeorge97@users.noreply.github.com>
2026-07-26 12:52:53 -0500

    Expand public API and set package version
    Updated public API to include additional components and set version.

--- src/s4_nnx/__init__.py
+++ src/s4_nnx/__init__.py
@@ -1,5 +1,39 @@
-"""s4_nnx package."""
+"""Public API for s4-nnx."""
 
-from .s4 import S4Regressor
+from .s4 import (
+    S4Block,
+    S4Config,
+    S4Layer,
+    S4LayerEnsemble,
+    S4Regressor,
+    SequenceBlockNNX,
+    StackedModelRegression,
+    causal_convolution,
+    create_model,
+    discrete_dplr,
+    kernel_dplr,
+    make_dplr_hippo,
+    make_hippo,
+    make_nplr_hippo,
+    scan_ssm,
+)
 
-__all__ = ["S4Regressor"]
+__all__ = [
+    "S4Block",
+    "S4Config",
+    "S4Layer",
+    "S4LayerEnsemble",
+    "S4Regressor",
+    "SequenceBlockNNX",
+    "StackedModelRegression",
+    "causal_convolution",
+    "create_model",
+    "discrete_dplr",
+    "kernel_dplr",
+    "make_dplr_hippo",
+    "make_hippo",
+    "make_nplr_hippo",
+    "scan_ssm",
+]
+
+__version__ = "0.1.0"
